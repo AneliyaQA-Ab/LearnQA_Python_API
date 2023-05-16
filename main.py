ee3b21a4c0eb10d9e5ba38edb5bdc05b25435a7f
AneliyaQA-Ab <aneliya.abolnik.fortest@gmail.com>
2023-05-16 17:24:32 +0300

    tests/HW_Lesson4_Ex16

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hello from Aneliya')
+import requests
+class TestCookie:
+    def test_cookie(self):
+        url = "https://playground.learnqa.ru/api/homework_cookie"
+
+        response = requests.get(url)
+
+        assert len(response.cookies) != 0, "No cookies in response" #проверка что кука возвращается
+
+        cookie_value = response.cookies['HomeWork']
+        expected_value = ('hw_value')
+
+        assert cookie_value == expected_value, "Wrong cookie value" #проверка на нужное значение
+
+
+
+
+
