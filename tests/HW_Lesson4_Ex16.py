338abbcfbf8bf83621b3170270e96d1df76fa318
AneliyaQA-Ab <aneliya.abolnik.fortest@gmail.com>
2023-07-20 16:58:04 +0300

    tests/HW_Lesson4_Ex16.py with adding allure tags

--- tests/HW_Lesson4_Ex16.py
+++ tests/HW_Lesson4_Ex16.py
@@ -1,16 +1,28 @@
+from logging import CRITICAL
+
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
+import allure
+
+@allure.epic("Authorization cases")
+@allure.link(name = "Документация API", url = "https://playground.learnqa.ru/api/map")
+@allure.issue('https://jira.kvantera.io/browse/QA-81', 'Пройти курс "Автоматизация API на python')
+
 
 class TestUserGet(BaseCase):
+    @allure.description("This test  authorize user w/o auth")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_get_user_details_not_auth(self):
-         response =  requests.get("https://playground.learnqa.ru/api/user/2") # захардкодили id  = 2
+         response = MyRequests.get("/user/2") # захардкодили id  = 2
 
          Assertions.assert_json_has_key(response, "username")
          Assertions.assert_json_has_not_key(response, "email")
          Assertions.assert_json_has_not_key(response, "firstName")
          Assertions.assert_json_has_not_key(response, "lastName")
 
+    @allure.description("This test  authorize as same user")
     def test_get_user_details_auth_as_same_user(self):
         data = {
             'email': 'vinkotov@example.com',
@@ -18,7 +30,7 @@ class TestUserGet(BaseCase):
         }
 
 
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        response1 = MyRequests.post("/user/login", data=data)
 
         auth_sid = self.get_cookie(response1, "auth_sid")
         token = self.get_header(response1, "x-csrf-token")
@@ -26,8 +38,8 @@ class TestUserGet(BaseCase):
         #print(response1.content)
 
 
-        response2 = requests.get(
-            f"https://playground.learnqa.ru/api/user/{user_id_from_auth_method}",
+        response2 = MyRequests.get(
+            f"/user/{user_id_from_auth_method}",
             headers={"x-csrf-token":token},
             cookies= {"auth_sid":auth_sid}
         )
@@ -35,7 +47,7 @@ class TestUserGet(BaseCase):
         expected_fields = ["username", "email", "firstName","lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
 
-
+    @allure.description("This test  authorize as same user")
     def test_get_user_details_auth_as_not_same_user(self):
         data = {
             'email': 'vinkotov@example.com',
@@ -43,15 +55,15 @@ class TestUserGet(BaseCase):
         }
 
 
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        response1 = MyRequests.post("/user/login", data=data)
 
         auth_sid = self.get_cookie(response1, "auth_sid")
         token = self.get_header(response1, "x-csrf-token")
         #print(response1.content)
 
 
-        response2 = requests.get(
-            f"https://playground.learnqa.ru/api/user/1", #захардкодили, для логина и пароля выше id = 2
+        response2 = MyRequests.get(
+            f"/user/1", #захардкодили, для логина и пароля выше id = 2
             headers={"x-csrf-token":token},
             cookies= {"auth_sid":auth_sid}
         )
